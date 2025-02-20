65533cb02f7a63d0dff4d3f91da544ee6638a814
CaioLeo01 <caio.m.c.leonardo@gmail.com>
2025-02-20 11:51:30 -0300

    Create TaskService

--- todosimple/src/main/java/com/caioleo/todosimple/services/TaskService.java
+++ todosimple/src/main/java/com/caioleo/todosimple/services/TaskService.java
@@ -3,11 +3,19 @@ package com.caioleo.todosimple.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.config.Task;
-import org.springframework.stereotype.Service;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.caioleo.todosimple.models.Task;
 import com.caioleo.todosimple.models.User;
 import com.caioleo.todosimple.repositories.TaskRepository;
+
+
+
+
+
+
 
 @Service
 public class TaskService {
@@ -23,11 +31,36 @@ public class TaskService {
 
         Optional<Task> task = this.taskRepository.findById(id);
         return task.orElseThrow(() -> new RuntimeException(
-        "Tarefa não encontrado! Id" + id + ", Tipo: " + Task.class.getName() ));
+         "Tarefa não encontrado! Id" + id + ", Tipo: " + Task.class.getName() ));
     }
-        
+
+    @Transactional    
     public Task create (Task obj){
-        User user - this.userService.findById(obj.getUser().getId());
+        User user = this.userService.findById(obj.getUser().getId());
+        obj.setId(null);
+        obj.setUser(user);
+        obj = this.taskRepository.save(obj);
+        return obj;
+
     }
     
+
+    @Transactional
+    public Task update (Task obj) {
+        Task newObj = findById(obj.getId());
+        newObj.setDescription (obj.getDescription());
+        return this.taskRepository.save(newObj);
+
+    }
+
+    public void delete (Long id) {
+        findById(id);
+        try {
+            this.taskRepository.deleteById(id);
+        } catch (Exception e) {
+           throw new RuntimeException ("Não é possivel excluir pois há entidade relacionadas!");
+        }
+
+    }
+
 }
