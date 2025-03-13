1c4fb23250e78683b759bbbf414695a209836235
CaioLeo01 <caio.m.c.leonardo@gmail.com>
2025-03-13 17:18:51 -0300

    Update

--- todosimple/src/main/java/com/caioleo/todosimple/controllers/TaskController.java
+++ todosimple/src/main/java/com/caioleo/todosimple/controllers/TaskController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.caioleo.todosimple.models.Task;
 import com.caioleo.todosimple.services.TaskService;
+import com.caioleo.todosimple.services.UserService;
 
 import jakarta.validation.Valid;
 
@@ -29,6 +31,9 @@ public class TaskController {
     @Autowired
     private TaskService taskService;
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping("/{id}")
     public ResponseEntity<Task> findById (@PathVariable Long id) {
         Task obj = this.taskService.findById(id);
@@ -36,8 +41,9 @@ public class TaskController {
 
     }
 
-    @GetMapping("/user/{userid}")
+    @GetMapping("/user/{userId}")
     public ResponseEntity<List<Task>> findAllByUserId(@PathVariable Long userId) {
+        userService.findById(userId);
         List<Task> objs = this.taskService.findAllByUserId(userId);
         return ResponseEntity.ok().body(objs);
     } 
@@ -52,7 +58,7 @@ public class TaskController {
     }
 
 
-    @PostMapping("/{id}")
+    @PutMapping("/{id}")
     @Validated
     public ResponseEntity<Void> update(@Valid @RequestBody Task obj, @PathVariable Long id) {
         obj.setId(id);
