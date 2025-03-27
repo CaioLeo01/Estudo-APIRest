334e1309db668d11c2e70dc816c58ea542cb2767
CaioLeo01 <caio.m.c.leonardo@gmail.com>
2025-03-27 16:22:27 -0300

    Add lombok

--- todosimple/src/main/java/com/caioleo/todosimple/models/Task.java
+++ todosimple/src/main/java/com/caioleo/todosimple/models/Task.java
@@ -11,12 +11,23 @@ import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import java.util.Objects;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
 
 
 
 @Entity
 @Table(name = Task.TABLE_NAME)
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
 public class Task {
     public static final String TABLE_NAME = "task";
         
@@ -34,89 +45,5 @@ public class Task {
     @NotEmpty
     @Size(min = 1, max = 255)
     private String description;
-
-
-    public Task() {
-    }
-
-    public Task(Long id, User user, String description) {
-        this.id = id;
-        this.user = user;
-        this.description = description;
-    }
-
-    public Long getId() {
-        return this.id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public User getUser() {
-        return this.user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public String getDescription() {
-        return this.description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Task id(Long id) {
-        setId(id);
-        return this;
-    }
-
-    public Task user(User user) {
-        setUser(user);
-        return this;
-    }
-
-    public Task description(String description) {
-        setDescription(description);
-        return this;
-    }
-
-   @Override
-public boolean equals(Object obj) {
-    if (obj == this) {
-        return true;  // Se os objetos forem o mesmo, retorna true
-    }
-    if (obj == null) {
-        return false; // Se o objeto comparado for nulo, retorna false
-    }
-    if (!(obj instanceof Task)) {
-        return false; // Se o objeto não for uma instância de User, retorna false
-    }
-
-    Task other = (Task) obj; // Faz o cast do objeto para a classe User
-
-    // Verifica se o id é diferente entre os objetos
-    if (this.id != null) {
-        if (other.id == null) {
-            return false; // Se um id é nulo e o outro não, retorna false
-        } else if (!this.id.equals(other.id)) {
-            return false; // Se os ids forem diferentes, retorna false
-        }
-    }
-
-    // Retorna true se os ids forem iguais ou ambos nulos
-    return Objects.equals(this.id, other.id) && Objects.equals(this.user, other.user) && Objects.equals(this.description, other.description);
-}
-
-@Override
-public int hashCode() {
-    final int prime = 31;
-    int result = 1;
-    result = prime * result + ((this.id == null) ? 0 : this.id.hashCode());
-    return result;  // Retorna apenas o código de hash do id, como no seu código original
-}
 
 }
--- todosimple/src/main/java/com/caioleo/todosimple/models/User.java
+++ todosimple/src/main/java/com/caioleo/todosimple/models/User.java
@@ -10,18 +10,27 @@ import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
-import java.util.Objects;
 import java.util.List;
 import java.util.ArrayList;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonProperty.Access;
 
 @Entity
 @Table(name = User.TABLE_NAME)
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
 
 public class User {
     public interface CreateUser {
@@ -52,87 +61,9 @@ public class User {
     private String password;
 
     @OneToMany(mappedBy = "user")
+    @JsonProperty(access = Access.WRITE_ONLY)
     private List<Task> tasks = new ArrayList<Task>();
 
-    public User() {
-
-    }
-
-    public User(Long id, String username, String password) {
-        this.id = id;
-        this.username = username;
-        this.password = password;
-
-    }
-
-    public Long getId() {
-        return this.id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return this.username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return this.password;
-    }
-
-    @JsonIgnore
-    public List<Task> getTasks() {
-        return this.tasks;
-    }
-
-    public void setTasks(List<Task> tasks) {
-        this.tasks = tasks;
-    }
-
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    @Override
-public boolean equals(Object obj) {
-    if (obj == this) {
-        return true;  // Se os objetos forem o mesmo, retorna true
-    }
-    if (obj == null) {
-        return false; // Se o objeto comparado for nulo, retorna false
-    }
-    if (!(obj instanceof User)) {
-        return false; // Se o objeto não for uma instância de User, retorna false
-    }
-
-    User other = (User) obj; // Faz o cast do objeto para a classe User
-
-    // Verifica se o id é diferente entre os objetos
-    if (this.id != null) {
-        if (other.id == null) {
-            return false; // Se um id é nulo e o outro não, retorna false
-        } else if (!this.id.equals(other.id)) {
-            return false; // Se os ids forem diferentes, retorna false
-        }
-    }
-
-    // Retorna true se os ids forem iguais ou ambos nulos
-    return Objects.equals(this.id, other.id) && Objects.equals(this.username, other.username) && Objects.equals(this.password, other.password);
-}
-
-@Override
-public int hashCode() {
-    final int prime = 31;
-    int result = 1;
-    result = prime * result + ((this.id == null) ? 0 : this.id.hashCode());
-    return result;  // Retorna apenas o código de hash do id, como no seu código original
-}
-
+    
 
 }
